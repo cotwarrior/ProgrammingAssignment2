dbe9eb70532affd2687be6a4abadc8814927205b
cotwarrior <wjge_usa@yahoo.com>
2015-04-15 11:22:04 -0500

    Implementation.
    Implementation of the cachematrix code as directed by the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the code for cached matrix inversion computation.
+## The code follows the same logic as the cached vector computation.
+#  1. makeCacheMatrix() makes a cached version of the original matrix.
+#  2. cachesolve() first checks if there is already an inversion.
+#     If not, inverse it. 
+#     If so, extract the cached result.
 
-## Write a short comment describing this function
-
+# makeCacheMatrix creates a special "matrix", which contains 4 component functions to
+# set or get the value of the matrix, or to set or get the inversion result.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is the function that computes matrix inversion.
+## The program first checks if there is already an inversion. If not, inverse it. 
+## If so, extract the cached result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
